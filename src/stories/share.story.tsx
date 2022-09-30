2e1e0cef714841df3b3ac9622603236e0c5f682b
SurenderLohia <surender.lohia.e@gmail.com>
2022-09-30 10:16:18 +0530

    Add base share component

---
+++ src/stories/share.story.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Share from "./components/Share";
+
+storiesOf("Share", module)
+  .add("Default", () => <Share></Share>)
