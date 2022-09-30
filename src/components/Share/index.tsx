02b506cafa237e678dc7ca7c4c4c694813362406
SurenderLohia <surender.lohia.e@gmail.com>
2022-09-30 11:55:29 +0530

    Add tailwind.css & update build scripts

---
+++ src/components/Share/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles/build.css';
+
+export default function () {
+  return <button className="bg-slate-400 text-blue-800" type="button">Share button</button>
+}
--- src/stories/share.story.tsx
+++ src/stories/share.story.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Share from "./components/Share";
+import Share from "../components/Share";
 
 storiesOf("Share", module)
   .add("Default", () => <Share></Share>)
