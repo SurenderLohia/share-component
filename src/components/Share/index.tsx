f8bd5f88383280347d314977d3f557e0d0ab1301
SurenderLohia <surender.lohia.e@gmail.com>
2022-10-01 13:31:59 +0530

    Add change view functionality

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/build.css';
 
 import { ShareIcon, GlobeIcon, ThumbLogo, HelpIcon, LinkIcon } from './Icons';
-import { AvatarProps, PersonItemProps } from './types';
+import { AvatarProps, PersonItemProps, ShareCurrentView } from './types';
 import avatar1 from '../../images/avatar-1.svg';
 import { groups, persons } from './data';
 
@@ -64,104 +64,139 @@ const Footer = (
 );
 
 export default function () {
+  const [currentView, setCurrentView] =
+    useState<ShareCurrentView>('share-button');
+
+  const changeView = (view: ShareCurrentView) => {
+    setCurrentView(view);
+  };
+
+  const handleShareButtonClick = () => {
+    setCurrentView('list');
+  };
+
+  const handleSearchClick = () => {
+    setCurrentView('person-selection');
+  };
+
+  const handleInviteClick = () => {
+    setCurrentView('share-button');
+  };
+
   return (
     <div className="share-container">
-      <button className={buttonClass} type="button">
-        Share
-        {ShareIcon}
-      </button>
+      {currentView === 'share-button' && (
+        <button
+          className={buttonClass}
+          type="button"
+          onClick={handleShareButtonClick}
+        >
+          Share
+          {ShareIcon}
+        </button>
+      )}
       {/* Invite-view */}
-      <div className="border border-gray-200 rounded-lg hidden">
-        <div className="flex border-b border-gray-200">
-          <div className="flex pl-5 pr-3 py-4 items-center">
-            {GlobeIcon}
-            <div className="ml-4">
-              <span className="font-main text-base text-gray-900 font-normal">
-                Share to web
-              </span>{' '}
-              <br />
-              <span className="font-main text-sm text-gray-500 font-normal">
-                Publish and share link with anyone
+      {currentView === 'list' && (
+        <div className="border border-gray-200 rounded-lg">
+          <div className="flex border-b border-gray-200">
+            <div className="flex pl-5 pr-3 py-4 items-center">
+              {GlobeIcon}
+              <div className="ml-4">
+                <span className="font-main text-base text-gray-900 font-normal">
+                  Share to web
+                </span>{' '}
+                <br />
+                <span className="font-main text-sm text-gray-500 font-normal">
+                  Publish and share link with anyone
+                </span>
+              </div>
+            </div>
+            {/* "https://medium.com/front-end-weekly/build-a-css-only-toggle-switch-using-tailwindcss-d2739882934" */}
+            <label className="relative flex justify-between items-center p-2 text-xl ml-auto">
+              <input
+                type="checkbox"
+                className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
+              />
+              <span className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-5"></span>
+            </label>
+          </div>
+          <div className="px-3 py-4 border-b border-gray-200">
+            <div className="flex mb-4">
+              <input
+                type="text"
+                className="border border-gray-300 rounded-l px-2 py-1 font-main flex-1"
+                placeholder="People, emails, groups"
+                onClick={handleSearchClick}
+              />
+              <span className="border border-gray-300 border-l-0 rounded-r px-4 py-2 font-main bg-gray-50 text-gray-700">
+                Invite
               </span>
             </div>
+            <div className="flex items-center">
+              <div className="flex">{ThumbLogo}</div>
+              <div className="ml-2">
+                <span className="font-main text-base text-gray-900 font-normal">
+                  Everyone at OSlash
+                </span>{' '}
+                <br />
+                <span className="font-main text-sm text-gray-500 font-normal">
+                  25 workspace members
+                </span>
+              </div>
+              <div className="group relative ml-auto">
+                <select className="p-2 text-xs text-gray-500">
+                  <option value="no-access">No access</option>
+                  <option value="full-access">Full access</option>
+                </select>
+              </div>
+            </div>
           </div>
-          {/* "https://medium.com/front-end-weekly/build-a-css-only-toggle-switch-using-tailwindcss-d2739882934" */}
-          <label className="relative flex justify-between items-center p-2 text-xl ml-auto">
-            <input
-              type="checkbox"
-              className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
-            />
-            <span className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-5"></span>
-          </label>
+          {Footer}
         </div>
-        <div className="px-3 py-4 border-b border-gray-200">
-          <div className="flex mb-4">
+      )}
+      {/* Person selection view */}
+      {currentView === 'person-selection' && (
+        <div className="border border-gray-200 rounded-lg">
+          <div className="flex px-3 py-3 bg-gray-50 rounded-t-lg items-center">
             <input
               type="text"
-              className="border border-gray-300 rounded-l px-2 py-1 font-main flex-1"
-              placeholder="People, emails, groups"
+              className="flex-1 text-sm text-gray-500 font-main focus:bottom-0 p-1 bg-transparent focus:outline-0"
+              placeholder="Search emails, names or groups"
+              value=""
             />
-            <span className="border border-gray-300 border-l-0 rounded-r px-4 py-2 font-main bg-gray-50 text-gray-700">
-              Invite
-            </span>
-          </div>
-          <div className="flex items-center">
-            <div className="flex">{ThumbLogo}</div>
-            <div className="ml-2">
-              <span className="font-main text-base text-gray-900 font-normal">
-                Everyone at OSlash
-              </span>{' '}
-              <br />
-              <span className="font-main text-sm text-gray-500 font-normal">
-                25 workspace members
-              </span>
-            </div>
-            <div className="group relative ml-auto">
-              <select className="p-2 text-xs text-gray-500">
+
+            <div className="flex ml-auto">
+              <select className="p-2 text-xs text-gray-500 bg-transparent mr-3">
                 <option value="no-access">No access</option>
                 <option value="full-access">Full access</option>
               </select>
+              <button
+                type="button"
+                className="border border-gray-200 rounded-lg text-sm text-gray-700 px-3 py-2"
+                onClick={handleInviteClick}
+              >
+                Invite
+              </button>
             </div>
           </div>
-        </div>
-        {Footer}
-      </div>
-      {/* Person selection view */}
-      <div className="border border-gray-200 rounded-lg">
-        <div className="flex px-3 py-3 bg-gray-50 rounded-t-lg items-center">
-          <input
-            type="text"
-            className="flex-1 text-sm text-gray-500 font-main focus:bottom-0 p-1 bg-transparent focus:outline-0"
-            placeholder="Search emails, names or groups"
-            value=""
-          />
-
-          <div className="flex ml-auto">
-            <select className="p-2 text-xs text-gray-500 bg-transparent mr-3">
-              <option value="no-access">No access</option>
-              <option value="full-access">Full access</option>
-            </select>
-            <button
-              type="button"
-              className="border border-gray-200 rounded-lg text-sm text-gray-700 px-3 py-2"
-            >
-              Invite
-            </button>
+          <div className="pl-6 pr-4 py-4">
+            {SectionTitle('Select a person')}
+            {persons.map((person) => (
+              <PersonItem
+                id={person.id}
+                name={person.name}
+                avatarUrl={avatar1}
+              />
+            ))}
+          </div>
+          <div className="pl-6 pr-4 py-4">
+            {SectionTitle('Select a group')}
+            {groups.map((person) => (
+              <PersonItem id={person.id} name={person.name} />
+            ))}
           </div>
         </div>
-        <div className="pl-6 pr-4 py-4">
-          {SectionTitle('Select a person')}
-          {persons.map((person) => (
-            <PersonItem id={person.id} name={person.name} avatarUrl={avatar1} />
-          ))}
-        </div>
-        <div className="pl-6 pr-4 py-4">
-          {SectionTitle('Select a group')}
-          {groups.map((person) => (
-            <PersonItem id={person.id} name={person.name} />
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
